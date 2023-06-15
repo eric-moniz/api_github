0ae8694e491ca8f15f49bbd8ffe5da41bba7f133
eric-moniz <ericmoniz@outlook.com.br>
2023-06-15 12:49:44 -0300

    Criando a tela repositories

---
+++ src/pages/RepositoiesPage/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RepositoriesPage() {
+  return <h1>RepositoriesPage</h1>;
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-// import MainPage from "./pages/Main";
 import MainPage from "./pages/MainPage";
+import RepositoriesPage from "./pages/RepositoiesPage";
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/repositories" element={<RepositoriesPage />} />
       <Route path="/" element={<MainPage />} />
     </Routes>
   );
