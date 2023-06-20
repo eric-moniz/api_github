6ce5874cb999238fe248f18712d8698c70fa4cd7
eric-moniz <ericmoniz@outlook.com.br>
2023-06-20 13:09:49 -0300

    Componente Filter - Parte 2

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Profile from "./Profile";
 import Filter from "./Filter";
 import Repositories from "./Repositories";
+import { getLangsFrom } from "../../services/api";
 
 import { Container, Sidebar, Main } from "./styles";
 
@@ -18,53 +19,46 @@ export default function RepositoriesPage() {
     location: "São José dos Campos - SP",
   };
 
-  // const repositories = [
-  //   {
-  //     name: "Repo 1",
-  //     description: "Descrição",
-  //     hmtl_url: "https://devsamurai.com.br",
-  //     language: "JavaScript",
-  //   },
-  //   {
-  //     name: "Repo 2",
-  //     description: "Descrição",
-  //     hmtl_url: "https://devsamurai.com.br",
-  //     language: "JavaScript",
-  //   },
-  //   {
-  //     name: "Repo 3",
-  //     description: "Descrição",
-  //     hmtl_url: "https://devsamurai.com.br",
-  //     language: "PHP",
-  //   },
-  //   {
-  //     name: "Repo 4",
-  //     description: "Descrição",
-  //     hmtl_url: "https://devsamurai.com.br",
-  //     language: "Ruby",
-  //   },
-  //   {
-  //     name: "Repo 5",
-  //     description: "Descrição",
-  //     hmtl_url: "https://devsamurai.com.br",
-  //     language: "CSS",
-  //   },
-  //   {
-  //     name: "Repo 6",
-  //     description: "Descrição",
-  //     hmtl_url: "https://devsamurai.com.br",
-  //     language: "TypeScript",
-  //   },
-  // ];
-
-  // Calculo dos filters
-  const languages = [
-    { name: "JavaScript", count: 2, color: "#f1c40f" },
-    { name: "PHP", count: 1, color: "#f1c40f" },
-    { name: "Ruby", count: 1, color: "#f1c40f" },
-    { name: "CSS", count: 1, color: "#f1c40f" },
-    { name: "TypeScript", count: 1, color: "#f1c40f" },
+  const repositories = [
+    {
+      name: "Repo 1",
+      description: "Descrição",
+      hmtl_url: "https://devsamurai.com.br",
+      language: "JavaScript",
+    },
+    {
+      name: "Repo 2",
+      description: "Descrição",
+      hmtl_url: "https://devsamurai.com.br",
+      language: "JavaScript",
+    },
+    {
+      name: "Repo 3",
+      description: "Descrição",
+      hmtl_url: "https://devsamurai.com.br",
+      language: "PHP",
+    },
+    {
+      name: "Repo 4",
+      description: "Descrição",
+      hmtl_url: "https://devsamurai.com.br",
+      language: "Ruby",
+    },
+    {
+      name: "Repo 5",
+      description: "Descrição",
+      hmtl_url: "https://devsamurai.com.br",
+      language: "CSS",
+    },
+    {
+      name: "Repo 6",
+      description: "Descrição",
+      hmtl_url: "https://devsamurai.com.br",
+      language: "TypeScript",
+    },
   ];
+
+  const languages = getLangsFrom(repositories);
 
   return (
     <Container>
